9f51adbdca06a750f0902cf5e0bbb6ad28cd6e4b
ekuznetsova <helenkuz1202@gmail.com>
2018-02-23 14:35:26 +0200

    added Assignment_4

---
+++ src/main/java/hw4/shapes/model/Circle.java
@@ -0,0 +1,38 @@
+package hw4.shapes.model;
+import java.util.Scanner;
+
+public class Circle extends AbstractShape {
+
+    private int radius;
+    private double squareFormula = Math.PI * radius * radius;
+    private double perimeterFormula = 2 * Math.PI * radius;
+
+
+    public Circle() {
+    }
+
+    @Override
+    public double calculateSquareOfShape() {
+        return squareFormula;
+    }
+
+    @Override
+    public double calculatePerimeterOfShape() {
+        return perimeterFormula;
+    }
+
+    @Override
+    public void createShape() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Вы выбрали круг.");
+        System.out.println("Введите радиус круга: ");
+        this.radius = sc.nextInt();
+    }
+
+    @Override
+    public String toString(){
+        return "Круг с радиусом " + radius + " имеет площадь " + calculateSquareOfShape()+
+                " и периметр " + calculatePerimeterOfShape();
+    }
+
+}
---
+++ src/main/java/hw4/shapes/model/Square.java
@@ -0,0 +1,35 @@
+package hw4.shapes.model;
+
+import java.util.Scanner;
+
+public class Square extends AbstractShape {
+
+    private int squareSide;
+    private double squareFormula = squareSide * squareSide;
+    private double perimeterFormula = 4 * squareSide;
+
+    public Square() {
+    }
+
+    @Override
+    public void createShape() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Вы выбрали квадрат.");
+        System.out.println("Введите сторону квадрата: ");
+        this.squareSide = sc.nextInt();
+    }
+
+    public double calculateSquareOfShape() {
+        return squareFormula;
+    }
+
+    public double calculatePerimeterOfShape() {
+        return perimeterFormula;
+    }
+
+    @Override
+    public String toString(){
+        return "Квадрат со стороной  " + squareSide + " имеет площадь " + calculateSquareOfShape()+
+                " и периметр " + calculatePerimeterOfShape();
+    }
+}
