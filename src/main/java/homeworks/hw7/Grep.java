8c66d71e196846d957463b2a902058dd310a2ec6
helen1202 <helenkuz1202@gmail.com>
2018-04-23 20:29:30 +0300

    hw 7 added

--- src/main/java/homeworks/hw7/Grep.java
+++ src/main/java/homeworks/hw7/Grep.java
@@ -2,28 +2,51 @@ package homeworks.hw7;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Grep {
 
-    public void grep(String phraseToFind, String filePath) throws IOException {
+    private final String filePath;
+    private final String phraseToFind;
+
+    public Grep(String filePath, String phraseToFind) {
+        this.filePath = filePath;
+        this.phraseToFind = phraseToFind;
+    }
+
+    public void grep() throws IOException {
+        String s;
+        Matcher matcher;
+        Pattern pattern = null;
+        boolean isRegExpr;
         try (LineNumberReader reader = new LineNumberReader
                 (Files.newBufferedReader
                         (Paths.get(filePath).toAbsolutePath()))) {
-
-            String s;
+            try {
+                pattern = Pattern.compile(phraseToFind);
+                isRegExpr = true;
+            } catch (PatternSyntaxException e) {
+                isRegExpr = false;
+            }
             while ((s = reader.readLine()) != null) {
-                if (s.contains(phraseToFind)) {
-                    System.out.println("*" +
-                            reader.getLineNumber() + "* " + s);
+                if (isRegExpr) {
+                    matcher = pattern.matcher(s);
+                    if (matcher.find()) {
+                        System.out.println("*" +
+                                reader.getLineNumber() + "* " + s);
+                    }
+                } else {
+                    if (s.contains(phraseToFind)) {
+                        System.out.println("*" +
+                                reader.getLineNumber() + "* " + s);
+                    }
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
 
--- src/main/java/homeworks/hw7/Program.java
+++ src/main/java/homeworks/hw7/Program.java
@@ -1,43 +1,47 @@
 package homeworks.hw7;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
-
-
+import java.nio.file.NoSuchFileException;
 
 public class Program {
     public static void main(String[] args) {
 
-        String filePath;
-        String stringToFind;
-        if(args.length == 2)
-        {
-           filePath = args[1];
-           stringToFind = args[0];
+        String filePath = null;
+        String stringToFind = null;
+        try {
+            filePath = args[0];
+            stringToFind = args[1];
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Incorrect input!\n First argument must be word to find and second one - file path.");
+            System.exit(1);
+        }
 
-            System.out.println("Find word: " +
-                    args[0] + " in file: " + args[1]);
-
+        System.out.println("Find word " +
+                    args[0] + " in file " + args[1]);
             try
             {
-               Grep grep = new Grep();
-               grep.grep(stringToFind,filePath);
-
+               Grep grep = new Grep(stringToFind,filePath);
+               grep.grep();
             }
             catch (AccessDeniedException e)
             {
-                System.out.println(e.toString());
-                System.exit(0);
+                System.err.println(e.toString());
             }
-            catch (IOException ex)
+            catch (NoSuchFileException e){
+                System.err.println(e.toString());
+            }
+            catch (FileNotFoundException e) {
+                System.err.println(e.toString());
+            }
+
+            catch (IOException e)
             {
-                System.out.println(ex.toString());
-                System.exit(0);
+                System.err.println(e.toString());
             }
         }
-        else
-            System.out.println("Incorrect input!\n First argument must be word to find and second one - file path.");
     }
-}
+
 
 
